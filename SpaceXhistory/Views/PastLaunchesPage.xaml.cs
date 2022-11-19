a19a3e7b26eaf11ea78dba07d14f80bb0257334a
Eduardo Reis <eduardoreisdev@gmail.com>
2022-11-19 20:52:42 -0300

    feat) create past launches list

--- SpaceXhistory/Views/PastLaunchesPage.xaml.cs
+++ SpaceXhistory/Views/PastLaunchesPage.xaml.cs
@@ -1,9 +1,23 @@
-﻿namespace SpaceXhistory.Views;
+﻿using SpaceXhistory.ViewModels;
+
+namespace SpaceXhistory.Views;
 
 public partial class PastLaunchesPage : ContentPage
 {
+	private readonly PastLaunchesViewModel _viewModel;
+
 	public PastLaunchesPage()
 	{
 		InitializeComponent();
-	}
+
+		_viewModel = new PastLaunchesViewModel();
+		BindingContext = _viewModel;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+		_viewModel.PopulateLatestLaunchs();
+    }
 }
